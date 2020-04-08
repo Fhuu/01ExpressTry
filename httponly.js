e030d4fe7c634ac4c0fd3b78e9e3ae809f87afd8
Diro Baloska <dirobaloska@outlook.com>
2020-04-08 20:36:28 +0200

    added requestParam for parameter transfer

--- httponly.js
+++ httponly.js
@@ -1,7 +1,10 @@
 "use strict!";
 var http = require("http");
+var url = require("url");
 
 http.createServer((req, res) => {
-    res.write("hello world!!!");
+    res.writeHead(200, {"Content-Type": "text/html"});
+    var urlRequest = url.parse(req.url, true).query;
+    res.write(urlRequest.name + " : " + urlRequest.password);
     res.end();
 }).listen(8080);
